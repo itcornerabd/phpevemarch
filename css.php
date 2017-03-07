3b5549e029d8a59ae471e18d61ea33ea4edad4b1
itcorner <info@itcorner.org.in>
2017-03-07 11:44:52 +0530

    css basics

---
+++ css.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Css Demo</title>
+
+	<style type="text/css">
+
+	</style>
+
+	<link rel="stylesheet" type="text/css" href="./style.css">
+
+</head>
+<body>
+
+<span > span 1</span>
+<span class="cls"> span 2 </span>
+
+<div class="cls"> div 1</div>
+<div id="div2"> div 2</div> 	
+
+<p style="font-size: 24px;"> para 1</p>
+<p class="cls"> para 2 </p>
+	<ul>
+		<li> <span> items1 </span></li>
+		<li> <span> item2 </span> </li>
+	</ul>
+
+
+</body>
+</html>
+
+
+
+
+
+
+
+
